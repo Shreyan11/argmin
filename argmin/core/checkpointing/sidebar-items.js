0f8a476ab3ac648f512aafbb38e0ed824500986b
Shreyan11 <Shreyan11@users.noreply.github.com>
2024-02-23 04:49:33 +0000

    deploy: 04025b75c5ea5b664444a66e8b1875d49b3a3187

---
+++ argmin/core/checkpointing/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["CheckpointingFrequency"],"trait":["Checkpoint"]};
---
+++ argmin/core/observers/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ObserverMode"],"struct":["Observers"],"trait":["Observe"]};
